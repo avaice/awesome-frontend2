9772ccd8b4fa51a04e47be2ea45113cfe56f7759
avaice <dev@cho-ice.xyz>
2024-11-29 07:49:32 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="font-lg p-4">awesome frontend</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
